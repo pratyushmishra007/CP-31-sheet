73f9c726d2db69c1cf20e576a44169a81eaeedce
Pratyush Mishra <pratyushmishra@Pratyushs-MacBook-Pro.local>
2026-06-22 23:54:26 +0530

    8th and 9th problem

---
+++ 800/Goals_of_victory.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+using pii = pair<int,int>;
+using pll = pair<long long,long long>;
+
+void solve(){
+    int n; cin>>n;
+    int sum = 0;
+    while(n>1){
+        int a; cin>>a;
+        sum+=a;
+        n--;
+    }
+    cout<<(-(sum))<<"\n";
+}
+
+static auto fast_io = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+int main() {
+    int T = 1;
+    if constexpr (true) {
+        cin >> T;
+    }
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
---
+++ 800/how_much_does_daytona_cost.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<long long>;
+using pii = pair<int,int>;
+using pll = pair<long long,long long>;
+
+void solve(){
+    int n, k; cin>>n>>k;
+    bool ans = false;
+    while(n--){
+        int a; cin>>a;
+        if(a == k)
+            ans = true;
+    }
+
+    cout<<(ans ? "YES\n" : "NO\n");
+}
+
+static auto fast_io = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+int main() {
+    int T = 1;
+    if constexpr (true) {
+        cin >> T;
+    }
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
